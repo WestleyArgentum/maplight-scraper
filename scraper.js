b1751e74bd3ff28e080be62875f74ad52922fea0
WestleyArgentum <WestleyArgentum@gmail.com>
2014-11-19 22:14:57 -0500

    pass in args to scraper

--- scraper.js
+++ scraper.js
@@ -16,7 +16,6 @@ var BILL_PREFIX_MAPPINGS = {
 };
 
 
-
 function billsFromMaplightData(file, cb) {
     var raw = fs.readFileSync(file);
     csv.parse(raw.toString(), function(err, data) {
@@ -122,11 +121,7 @@ function downloadBillDescription(session, prefix, num, actionId, outFile) {
     });
 }
 
-
-billsFromMaplightData('MapLight_112thBills 20140822.csv', downloadBillDescriptions);
-
-
-function downloadBills(versionedBills) {
+function downloadBillsContributions(versionedBills) {
     var currTimeout = 0;
     var num = 0;
     for (var actionId in versionedBills) {
@@ -135,24 +130,35 @@ function downloadBills(versionedBills) {
         }
 
         var bill = versionedBills[actionId];
-        var outFile = '' + bill['session'] + '-' + bill['prefix'] + '-' + bill['num'] + '-' + actionId + '.csv'
+        var outFile = '' + bill['session'] + '-' + bill['prefix'] + '-' + bill['num'] + '-' + actionId + '-contributions.csv'
 
         if (fs.existsSync(outFile)) {
             continue;
         }
 
-        setTimeout(downloadBill, currTimeout, bill['session'], bill['prefix'], bill['num'], actionId, outFile);
-        currTimeout += 1000 * 15;
+        setTimeout(downloadBillContributions, currTimeout, bill['session'], bill['prefix'], bill['num'], actionId, outFile);
+        currTimeout += 1000 * 10;
         num += 1;
     }
 
-    console.log('Downloading ', num, 'bills...')
+    console.log('Downloading ', num, 'bills contributions...')
 }
 
-
-function downloadBill(session, prefix, num, actionId, outFile) {
+function downloadBillContributions(session, prefix, num, actionId, outFile) {
     console.log('Downloading bill ' + prefix + ' ' + num + ' ' + actionId);
 
     var req = 'http://maplight.org/us-congress/bill/' + session + '-' + prefix + '-' + num + '/' + actionId + '/download.csv';
     request(req).pipe(fs.createWriteStream(outFile));
 }
+
+bulk_filepath = process.argv[2];
+if (!fs.existsSync(bulk_filepath)) {
+    console.log('Error reading bulk data file. The first argument to this script should be a path to bulk maplight data.' +
+        ' See `maplight-scraper/maplight-data/`');
+    return;
+}
+
+data_type = process.argv[3] || 'descriptions';
+downloader_fn = data_type == 'descriptions' ? downloadBillDescriptions : downloadBillsContributions;
+
+billsFromMaplightData(bulk_filepath, downloader_fn);
